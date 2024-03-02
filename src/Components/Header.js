a4eef3ea96049fe721e8fc268041b5593ac571e6
ivanpanchenko04 <89913080+ivanpanchenko04@users.noreply.github.com>
2024-03-02 11:37:30 +0200

    added login form validation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Button, Container, Form, FormControl, Modal, Nav, Navbar} from "react-bootstrap";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import logo from "../img/logo192.png";
@@ -11,6 +11,54 @@ export default function Header() {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [emailDirty, setEmailDirty] = useState(false)
+    const [passwordDirty, setPasswordDirty] = useState(false)
+    const [emailError, setEmailError] = useState('Email не може бути порожнім')
+    const [passwordError, setPasswordError] = useState('Пароль не може бути порожнім')
+    const [formValid, setFormValid] = useState(false)
+    useEffect ( () => {
+        if (emailError || passwordError) {
+            setFormValid(false)
+        } else {
+            setFormValid(true)
+        }
+    },[emailError, passwordError])
+
+
+    const emailHandler = (e) => {
+        setEmail(e.target.value)
+        const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (!re.test(String(e.target.value.toLowerCase()))) {
+            setEmailError('Некоректний email')
+        } else {
+            setEmailError('')
+        }
+    }
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value)
+        if (e.target.value.length < 3 || e.target.length > 8) {
+            setPasswordError('Пароль повинен мати не менше 3 і не більше 8 символів')
+            if (!e.target.value) {
+                setPasswordError('Пароль не може бути порожнім')
+            }
+        } else {
+            setPasswordError('')
+        }
+    }
+
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'email':
+                setEmailDirty(true)
+                break
+            case 'password':
+                setPasswordDirty(true)
+                break
+        }
+    }
 
     return (
         <>
@@ -53,20 +101,29 @@ export default function Header() {
                     <Form>
                         <Form.Group controlId="fromBasicEmail">
                             <Form.Label>Email Address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            {(emailDirty && emailError) && <div style={{color: "red"}}>{emailError}</div>}
+                            <Form.Control onChange={e => emailHandler(e)} name="email"
+                                          value={email} onBlur={e => blurHandler(e)} type="email"
+                                          placeholder="Enter email"/>
                             <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                             </Form.Text>
                         </Form.Group>
+
                         <Form.Group controlId="fromBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Enter password">
+                            {(passwordError && passwordDirty) && <div style={{color: "red"}}>{passwordError}</div>}
+                            <Form.Control onChange={e => passwordHandler(e)} name="password"
+                                          value={password} onBlur={e => blurHandler(e)}
+                                          type="password" placeholder="Enter password">
                             </Form.Control>
                         </Form.Group>
+
                         <Form.Group controlId="fromBasicCheckbox">
                             <Form.Check type="checkbox" label="Remember me"/>
                         </Form.Group>
-                        <Button variant="primary" type="submit">
+
+                        <Button disabled={!formValid} variant="primary" type="submit">
                             Submit
                         </Button>
                     </Form>
